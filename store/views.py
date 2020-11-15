2dfd1991f83c3de3d75a33c4e8d340f7d18c9cc9
Axel González Rodríguez <ed.dante.axel@gmail.com>
2020-11-14 19:37:15 -0600

    Update views.py

--- store/views.py
+++ store/views.py
@@ -11,3 +11,6 @@ def storevw(request, numero_id):
 	lista_serv=Servicios.objects.order_by('pub_date')[:6]
 	salida=get_object_or_404(Servicios, pk=numero_id)
 	return render(request, 'store/tienda.html', {'lista_serv':lista_serv, 'salida':salida, 'hora':hora} )
+
+def versiones(request):
+	return render(request, 'store/versiones.html')
